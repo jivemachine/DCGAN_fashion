664a681e66a3ce8af450202ed4d71923284abc44
jivemachine <the.jeremy.cobb@gmail.com>
2020-12-03 14:17:56 -0600

    added build_discriminator script to DCGAN file

--- dcgan.py
+++ dcgan.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 from tf import keras
 from tensorflow.keras.models import Sequential()
 from tensorflow.keras.layers import BatchNormalization()
-from tensorflow.keras.layers import Conv2dTranspose
-from tensorflow.keras.layers import Conv2d
+from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import LeakyReLU
 from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import Flatten
@@ -38,4 +38,27 @@ class DCGAN:
         return model
 
 
-        
+   @staticmethod
+   def build_discriminator(width, height, depth, alpha=0.2):
+       # initalizing the model
+       model = Sequential()
+       inputshape = (height, width, depth)
+
+       # first layer
+       model.add(Conv2D(32, (5, 5), padding="same", strides=(2,2), input_shape=inputshape))
+       model.add(LeakyReLU(alpha=alpha))
+
+       #second layer
+       model.add(Conv2D(64, (5, 5), padding="same", strides=(2,2)))     
+       model.add(LeakyReLU(alpha=alpha))
+
+       # third layer
+       model.add(Flatten())
+       model.add(Dense(512))
+       model.add(LeakyReLU(alpha=alpha))
+
+       # output layer
+       model.add(Dense(1))
+       model.add(Activation("sigmoid"))
+
+       return model
